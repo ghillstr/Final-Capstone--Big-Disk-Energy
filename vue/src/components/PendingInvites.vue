a49c55540986848556dff6b7ad9a130f5f2f0d17
Kelsey <kmoslen@gmail.com>
2020-12-17 15:41:44 -0500

    deleted commented out stuff in pending invites

--- vue/src/components/PendingInvites.vue
+++ vue/src/components/PendingInvites.vue
@@ -7,36 +7,6 @@
       v-bind:key="invite.username"
     >
       <p>{{ invite.leagueName }}</p>
-      <!-- <div>
-        <b-form-select
-          @change="updateInvite"
-          v-model="invites.statusId"
-          name="inviteStatusId"
-        >
-          <option value="2">Accept</option>
-          <option value="3">Reject</option>
-        </b-form-select>
-        <button class="button" type="submit" @click="updateTheInvite()">
-          SUBMIT
-        </button>
-      </div> -->
-
-      <!-- <div class="btn-group" role="group" aria-label="Basic example">
-        <button
-          type="button"
-          class="btn btn-secondary"
-          @click="updateTheInvite()"
-        >
-          Accept
-        </button>
-        <button
-          type="button"
-          class="btn btn-secondary"
-          @click="updateTheInvite()"
-        >
-          Reject
-        </button>
-      </div> -->
 
       <b-form-group v-slot="{ ariaDescribedby }">
         <b-form-radio
@@ -55,20 +25,6 @@
       <button class="button" type="submit" @click="updateTheInvite(invite)">
         SUBMIT
       </button>
-
-      <!-- <b-form-checkbox
-        id="checkbox-1"
-        v-model="invites.statusId"
-        name="checkbox-1"
-        value="2"
-        unchecked-value="3"
-      >
-        Accept Invitation
-      </b-form-checkbox>
-
-      <button class="button" type="submit" @click="updateTheInvite()">
-        SUBMIT
-      </button> -->
     </div>
   </div>
 </template>
